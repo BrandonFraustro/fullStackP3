556d9c8f69eef597ec3825b4696cce004979f2cd
Brandon Fraustro <Brandonfraustro20@gmail.com>
2023-08-20 15:57:00 -0600

    Manejo de errores middelware en backend

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,6 @@ const mongoose = require('mongoose')
 const Phonebook = require('./models/phonebook')
 
 const app = express() 
-
 app.use(express.json())
 app.use(cors())
 
@@ -22,31 +21,45 @@ const morganTokens = morgan(function (tokens, req, res){
 })
 app.use(morganTokens);
 
-const errorMorgan = morgan('combined', {
-    skip: function (req, res) { return res.statusCode < 400 }
-})
-
 app.get('/api/persons', (request, response) => {
     Phonebook.find({}).then(phone => {
         response.json(phone)
     })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-    if (person) {
-        response.json(person)
-    } else {
-        response.status(404).end()
-    }
+app.get('/api/persons/:id', (request, response, next) => {
+    Phonebook.findById(request.params.id)
+    .then(phonebook => {
+        if(phonebook) {
+            response.json(phonebook)
+        } else {
+            response.status(404).end()
+        }
+    })
+    .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
+app.delete('/api/persons/:id', (request, response, next) => {
+    Phonebook.findByIdAndRemove(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
+})
 
-    response.status(204).end()
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+
+    const phonebook = {
+        name: body.name,
+        number: body.number,
+    }
+
+    Phonebook.findByIdAndUpdate(request.params.id, phonebook, { new: true })
+        .then(updatedPhonebook => {
+            response.json(updatedPhonebook)
+        })
+        .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
@@ -68,7 +81,21 @@ app.post('/api/persons', (request, response) => {
     })   
 })
 
+const errorMorgan = morgan('combined', {
+    skip: function (req, res) { return res.statusCode < 400 }
+})
 app.use(errorMorgan)
+
+const errorHandler = (error, request, response, next) => {
+    console.log(error.message)
+
+    if(error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
