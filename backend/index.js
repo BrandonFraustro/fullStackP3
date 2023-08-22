af0d79c4ba568c61b57ff6ac64a5b2645e88e953
Brandon Fraustro <Brandonfraustro20@gmail.com>
2023-08-21 21:10:23 -0600

    adding mongoose-unique-validator

--- backend/index.js
+++ backend/index.js
@@ -62,7 +62,7 @@ app.put('/api/persons/:id', (request, response, next) => {
         .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
 
     if (!body.name || !body.number) {
@@ -75,10 +75,12 @@ app.post('/api/persons', (request, response) => {
         name: body.name,
         number: body.number,
     })
-    console.log(person);
-    person.save().then(savedPerson => {
+
+    person.save()
+    .then(savedPerson => {
         response.json(savedPerson)
-    })   
+    })
+    .catch(error => next(error))
 })
 
 const errorMorgan = morgan('combined', {
@@ -91,6 +93,8 @@ const errorHandler = (error, request, response, next) => {
 
     if(error.name === 'CastError') {
         return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
     }
 
     next(error)
